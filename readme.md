3f1f23b79492808d12a093e2ac6998802f9d5f32
0x-hazel <hazelmorris@mf.me>
2025-06-02 10:07:59 +0000

    Implemented further required structure and moved into dev container

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Libris
+
+Libris is a blog-authoring website similar to medium, in that it provides a way for authors
+to create articles and grow an audience.
+
+---
+
+## Running the project
+
+```
+npm install
+npm start
+```
+
+This will run the project in development mode. To run unit tests, instead run `npm run test`
---
+++ server/src/server.test.js
@@ -0,0 +1,12 @@
+import request from "supertest";
+import { expect, test } from "vitest";
+import app from "./server.js";
+
+test("Hello world", async () => {
+    const test = await request(app)
+        .get("/")
+        .send();
+    console.log(test);
+    expect(test.statusCode).toEqual(200);
+    expect(test.text).toEqual("Hello, world!");
+});
